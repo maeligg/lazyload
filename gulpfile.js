86c5315a72d2aeee256c8b7f4d7ec422d15485e8
Andrea Verlicchi <verlicchia@yoox.net>
2018-08-02 18:46:10 +0200

    Multiple output formats: es5, amd and umd

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,42 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
-var rollup = require("gulp-rollup");
+var rollup = require("gulp-better-rollup");
+var sourcemaps = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
-var babel = require("gulp-babel");
-var uglify = require("gulp-uglify");
+var babel = require("rollup-plugin-babel");
 
 var destFolder = "./dist";
+var pkg = require("./package.json");
 
 gulp.task("default", function() {
 	process.env.NODE_ENV = "release";
 	return (
 		gulp.
-			src("./src/**/*.js").
+			src("./src/lazyload.js").
+			pipe(sourcemaps.init()).
 			// ----------- linting --------------
 			pipe(eslint()).
 			pipe(eslint.format()).
 			pipe(eslint.failAfterError()). // --> failing if errors
 			// ----------- rolling up --------------
 			pipe(
-				rollup({
-					output: { name: "LazyLoad", format: "amd" },
-					input: "./src/lazyload.js"
-				})
+				rollup({ plugins: [babel()] }, [
+					{
+						file: "dist/lazyload-es2015.js",
+						format: "es"
+					},
+					{
+						file: "dist/lazyload-umd.js",
+						format: "umd"
+					},
+					{
+						file: "dist/lazyload-amd.js",
+						format: "amd"
+					}
+				])
 			).
-			pipe(rename("lazyload.es2015.js")).
-			pipe(gulp.dest(destFolder)). // --> writing rolledup
-			// ----------- babelizing --------------
-			pipe(babel()).
-			pipe(rename("lazyload.js")).
-			pipe(gulp.dest(destFolder)). // --> writing babelized ES5
-			// ----------- minifying --------------
-			pipe(uglify()).
-			pipe(rename("lazyload.min.js")).
-			pipe(gulp.dest(destFolder)) // --> writing uglified
+			pipe(sourcemaps.write("")).
+			pipe(gulp.dest(destFolder))
 	);
 });
 
